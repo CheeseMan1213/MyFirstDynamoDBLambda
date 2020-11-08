92da0ae8bf958801d1ee3e3d49382da26ddc3ea8
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2020-11-08 17:48:22 -0600

    I changed the type inside the '<>' from '<HealthFirstMemberRequest, HealthFirstMemberResponse>' to '<StudentRequest, ItemCollection<ScanOutcome>>', because I am trying to make this code work, and these types need to match up with the ones from the member() method.

--- src/main/java/com/healthfirst/memberfunction/AwsLambdaHandler.java
+++ src/main/java/com/healthfirst/memberfunction/AwsLambdaHandler.java
@@ -1,7 +1,14 @@
 package com.healthfirst.memberfunction;
 
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
+import com.healthfirst.memberfunction.entity.StudentRequest;
+import com.healthfirst.memberfunction.entity.StudentResponse;
+
 import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
 
-public class AwsLambdaHandler extends SpringBootRequestHandler<HealthFirstMemberRequest, HealthFirstMemberResponse> {
-
+//public class AwsLambdaHandler extends SpringBootRequestHandler<StudentRequest, StudentResponse> {
+//public class AwsLambdaHandler extends SpringBootRequestHandler<HealthFirstMemberRequest, HealthFirstMemberResponse> {
+public class AwsLambdaHandler extends SpringBootRequestHandler<StudentRequest, ItemCollection<ScanOutcome>> {
+	//
 }
