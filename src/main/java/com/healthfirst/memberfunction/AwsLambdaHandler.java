efdb3348ef9439034d21ede3a121ddcaee379a9d
James Hawley <iliketofeelcheesetootoo@yahoo.com>
2020-11-09 14:11:32 -0600

    I changed the <StudentRequest, ItemCollection<ScanOutcome>> to <StudentRequest, List<String>>.

--- src/main/java/com/healthfirst/memberfunction/AwsLambdaHandler.java
+++ src/main/java/com/healthfirst/memberfunction/AwsLambdaHandler.java
@@ -1,5 +1,6 @@
 package com.healthfirst.memberfunction;
 
+import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.ItemCollection;
 import com.amazonaws.services.dynamodbv2.document.ScanOutcome;
 import com.healthfirst.memberfunction.entity.StudentRequest;
@@ -7,8 +8,14 @@ import com.healthfirst.memberfunction.entity.StudentResponse;
 
 import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
 
+import java.util.Iterator;
+import java.util.List;
+
 //public class AwsLambdaHandler extends SpringBootRequestHandler<StudentRequest, StudentResponse> {
 //public class AwsLambdaHandler extends SpringBootRequestHandler<HealthFirstMemberRequest, HealthFirstMemberResponse> {
-public class AwsLambdaHandler extends SpringBootRequestHandler<StudentRequest, ItemCollection<ScanOutcome>> {
+//public class AwsLambdaHandler extends SpringBootRequestHandler<StudentRequest, ItemCollection<ScanOutcome>> {
+//public class AwsLambdaHandler extends SpringBootRequestHandler<StudentRequest, Iterator<Item>> {
+public class AwsLambdaHandler extends SpringBootRequestHandler<StudentRequest, List<String>> {
+
 	//
 }
